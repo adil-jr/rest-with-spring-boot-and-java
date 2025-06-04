da0980a15e83f42e2dacec181a319c2515d53010
adil-jr <adil.jr@outlook.com>
2025-06-04 16:19:23 -0300

    feat: case study of path params and exception handling without using dependency injection and how this makes the code unnecessarily confusing

---
+++ 02_UnderstandingPathParamsandExceptionHandlinginSpring Boot/src/main/java/io/github/adil_jr/request/converters/NumberConverter.java
@@ -0,0 +1,18 @@
+package io.github.adil_jr.request.converters;
+
+import io.github.adil_jr.exception.UnsupportedMathOperationException;
+
+public class NumberConverter {
+
+    public static Double convertToDouble (String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) throw  new UnsupportedMathOperationException("Please set a numeric value!");
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric (String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
